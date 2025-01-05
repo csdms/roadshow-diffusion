01bb95a23e7c494260e9423037cc4782a88afc45
Mark Piper <mark.piper@colorado.edu>
2025-01-04 19:19:15 -0700

    Add unit tests

---
+++ tests/test_diffusion.py
@@ -0,0 +1,39 @@
+"""Test the diffusion module."""
+import math
+
+import numpy as np
+from roadshow_diffusion.diffusion import calculate_time_step, set_initial_profile, make_grid, solve1d
+
+DOMAIN_SIZE = 100
+GRID_SPACING = 1.0
+DIFFUSIVITY = 1.0
+TIME_STEP = 0.5
+TOLERANCE = 0.01
+ZMAX = 500.0
+
+
+def test_time_step():
+    time_step = calculate_time_step(GRID_SPACING, DIFFUSIVITY)
+    assert type(time_step) is float
+    assert math.isclose(time_step, TIME_STEP, rel_tol=TOLERANCE)
+
+
+def test_initial_profile_defaults():
+    z = set_initial_profile()
+    assert type(z) is np.ndarray
+    assert len(z) == DOMAIN_SIZE
+    assert math.isclose(z.max(), ZMAX, rel_tol=TOLERANCE)
+
+
+def test_make_grid():
+    pass
+
+
+def test_solve1d():
+    z = np.zeros(DOMAIN_SIZE)
+    z[DOMAIN_SIZE // 2] = ZMAX
+    solve1d(z, time_step=TIME_STEP)
+    assert type(z) is np.ndarray
+    assert len(z) == DOMAIN_SIZE
+    assert z.max() < ZMAX
+    assert z.sum() == ZMAX
