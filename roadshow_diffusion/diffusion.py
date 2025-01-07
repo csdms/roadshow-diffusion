60c5a0c56fb22ee25a16983a8e32a1fe1a96063e
Mark Piper <mark.piper@colorado.edu>
2025-01-07 17:25:37 -0600

    Rename diffusion_example to diffusion_model

--- roadshow_diffusion/diffusion.py
+++ roadshow_diffusion/diffusion.py
@@ -1,29 +1,11 @@
-# # A 1D diffusion model
-# Here we develop a one-dimensional model of diffusion.
-# It assumes a constant diffusivity.
-# It uses a regular grid.
-# It has fixed boundary conditions.
-# The diffusion equation:
-# 
-# $$ \frac{\partial C}{\partial t} = D\frac{\partial^2 C}{\partial x^2} $$
-# 
-# The discretized version of the diffusion equation we'll solve with our model:
-# 
-# $$ C^{t+1}_x = C^t_x + {D \Delta t \over \Delta x^2} (C^t_{x+1} - 2C^t_x + C^t_{x-1}) $$
-# 
-# This is the explicit FTCS scheme as described in Slingerland and Kump (2011).
-
-# We'll use two libraries, [NumPy](https://numpy.org/) (for arrays) and [Matplotlib](https://matplotlib.org/) (for plotting), that aren't a part of the core Python library.
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-# Write a function to calculate the time step of the model.
 def calculate_time_step(grid_spacing, diffusivity):
     return 0.5 * grid_spacing**2 / diffusivity
 
 
-# Make a function that creates the initial concentration profile.
 def set_initial_profile(grid_size=100, boundary_left=500, boundary_right=0):
     profile = np.empty(grid_size)
     profile[: grid_size // 2] = boundary_left
@@ -31,7 +13,6 @@ def set_initial_profile(grid_size=100, boundary_left=500, boundary_right=0):
     return profile
 
 
-# Make a function to plot the concentration profile.
 def plot_profile(concentration, grid, color="r"):
     plt.figure()
     plt.plot(grid, concentration, color)
@@ -40,13 +21,11 @@ def plot_profile(concentration, grid, color="r"):
     plt.title("Concentration profile")
 
 
-# Write a function to set up the model grid.
 def make_grid(domain_size, grid_spacing):
     grid = np.arange(start=0, stop=domain_size, step=grid_spacing)
     return (grid, len(grid))
 
 
-# Finally, write a function to solve the diffusion equation, advancing the model by one time step.
 def solve1d(concentration, grid_spacing=1.0, time_step=1.0, diffusivity=1.0):
     """Solve the one-dimensional diffusion equation with fixed boundary conditions.
 
@@ -82,8 +61,7 @@ def solve1d(concentration, grid_spacing=1.0, time_step=1.0, diffusivity=1.0):
     concentration[1:-1] += diffusivity * time_step / grid_spacing**2 * centered_diff[1:-1]
 
 
-# Run the model in an example.
-def diffusion_example():
+def diffusion_model():
     D = 100
     Lx = 7
     dx = 0.5
@@ -99,5 +77,5 @@ def diffusion_example():
 
 
 if __name__ == "__main__":
-    print("Diffusion model example")
-    diffusion_example()
+    print("Diffusion model")
+    diffusion_model()
